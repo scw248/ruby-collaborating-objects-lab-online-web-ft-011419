7ad770249d2338178561987df0fe3f329f24904e
Stephen Wright <scw248@gmail.com>
2019-01-28 22:24:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,13 @@ attr_accessor :name, :artist
     @@all << self
   end
   
+  def artist(artist_name)
+    
+  end
   
   def self.new_by_filename(file_name)
     song = self.new(file_name)
     song.name = file_name.split(" - ")[1]
-    # song.artist.name = file_name.split(" - ")[0]
     song
   end
   
